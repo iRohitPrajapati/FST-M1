a1fbf38b5009654d9b2bfb45846f424a11ae719e
Home <80896041+iRohitPrajapati@users.noreply.github.com>
2021-08-01 20:22:00 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+name = input("Enter your name ")
+age = input("Enter your age ")
+temp = str(100-int(age))
+print("Hello "+name+", you are attaining 100 year after "+temp+" years")
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,5 @@
+num = input("Enter a numer ")
+if int(num)%2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
